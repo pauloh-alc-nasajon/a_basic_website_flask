989186b43e5ee7c9049488e8712566026d208314
pauloh-alc-nasajon <pauloalencar@nasajon.com.br>
2022-11-17 03:33:00 -0300

    Criando redirecionamento

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -12,6 +12,11 @@ def home():
 @app.route('/<name>')
 def user(name):
     return f'Hello {name}!'
+
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
