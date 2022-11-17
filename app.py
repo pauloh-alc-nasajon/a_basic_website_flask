5b1195d2e27defc4142adcae31d0a4e380510b42
pauloh-alc-nasajon <pauloalencar@nasajon.com.br>
2022-11-17 14:08:55 -0300

    Criando uma sessão

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, request
+from contextlib import redirect_stderr
+
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = 'hello'
 
 
 # Defining the home page of our site
@@ -9,9 +13,13 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/<usr>')
-def user(usr):
-    return f'<h1>{usr}</h1>'
+@app.route('/user')
+def user():
+    if 'user' in session:
+        user = session['user']
+        return f'<h1>{user}</h1>'
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/admin')
@@ -22,10 +30,21 @@ def admin():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
+        session.permanent = True
         user = request.form['nm']
-        return redirect(url_for('user', usr=user))
+        session['user'] = user
+        return redirect(url_for('user'))
     else:
+        if 'user' in session:
+            return redirect(url_for('user'))
+
         return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
