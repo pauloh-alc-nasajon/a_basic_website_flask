c468f7b7f35a7f7edbfdfc15b1f414c2c1f4ae3a
pauloh-alc-nasajon <pauloalencar@nasajon.com.br>
2022-11-17 03:47:17 -0300

    Renderizando HTML
    * Foi criado o diretório templates/;
    * Dentro de templates/ foi criado o arquivo index.html;
    * Foi importado a função render_template;
    * Ao acessar a rota / o template vai ser renderizado.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Defining the home page of our site
 @app.route('/')  # this sets the route to this page
 def home():
-    return "Hello! this is the main page <h1>HELLO</h1>"  # some basic inline html
+    return render_template('index.html')
 
 
 @app.route('/<name>')
